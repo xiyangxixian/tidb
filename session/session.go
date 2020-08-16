f630b3fe2ec3833403f34db0dedd037ebec867ac
yangming <yangming6@xiaomi.com>
2020-08-16 18:30:47 +0800

    format space

--- session/session.go
+++ session/session.go
@@ -1505,7 +1505,7 @@ func (s *session) NewTxn(ctx context.Context) error {
 	}
 
 	txn, err := s.store.Begin()
-    logutil.Logger(ctx).Info("hello transaction")
+	logutil.Logger(ctx).Info("hello transaction")
 	if err != nil {
 		return err
 	}
